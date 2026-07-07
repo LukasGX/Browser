121da315b6c86958873e9cce55ec7f31be05d1ce
LukasGX <lukas@grambs.info>
2026-07-07 18:47:34 +0200

    initial commit

---
+++ Browser.Tests/UnitTest1.cs
@@ -0,0 +1,339 @@
+﻿using Xunit;
+
+namespace Browser.Tests;
+
+public class HtmlTokenizerTests
+{
+    private readonly HtmlTokenizer tokenizer = new();
+
+
+    [Fact]
+    public void ParsesSimpleTag()
+    {
+        var tokens = tokenizer.Tokenize("<h1>Hello</h1>");
+
+        Assert.Equal(3, tokens.Count);
+
+        Assert.Equal(TokenType.StartTag, tokens[0].Type);
+        Assert.Equal("h1", tokens[0].Value);
+
+        Assert.Equal(TokenType.Text, tokens[1].Type);
+        Assert.Equal("Hello", tokens[1].Value);
+
+        Assert.Equal(TokenType.EndTag, tokens[2].Type);
+        Assert.Equal("h1", tokens[2].Value);
+    }
+
+
+    [Fact]
+    public void ParsesAttributes()
+    {
+        var tokens = tokenizer.Tokenize(
+            """<a href="https://example.com" target="_blank">Link</a>"""
+        );
+
+        Token token = tokens[0];
+
+        Assert.Equal("a", token.Value);
+
+        Assert.Equal(
+            "https://example.com",
+            token.Attributes["href"]
+        );
+
+        Assert.Equal(
+            "_blank",
+            token.Attributes["target"]
+        );
+    }
+
+
+    [Fact]
+    public void ParsesSelfClosingTags()
+    {
+        var tokens = tokenizer.Tokenize(
+            """<img src="test.png">"""
+        );
+
+        Assert.Single(tokens);
+
+        Assert.Equal(
+            TokenType.SelfClosingTag,
+            tokens[0].Type
+        );
+
+        Assert.Equal(
+            "test.png",
+            tokens[0].Attributes["src"]
+        );
+    }
+
+
+    [Fact]
+    public void ParsesDoctype()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<!doctype html>"
+        );
+
+        Assert.Single(tokens);
+        Assert.Equal(TokenType.Doctype, tokens[0].Type);
+    }
+
+
+    [Fact]
+    public void ParsesComments()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<!-- hello -->"
+        );
+
+        Assert.Single(tokens);
+
+        Assert.Equal(
+            TokenType.Comment,
+            tokens[0].Type
+        );
+
+        Assert.Equal(
+            " hello ",
+            tokens[0].Value
+        );
+    }
+
+
+    [Fact]
+    public void ParsesText()
+    {
+        var tokens = tokenizer.Tokenize(
+            "Hello World"
+        );
+
+        Assert.Single(tokens);
+
+        Assert.Equal(
+            TokenType.Text,
+            tokens[0].Type
+        );
+
+        Assert.Equal(
+            "Hello World",
+            tokens[0].Value
+        );
+    }
+
+    [Fact]
+    public void ParsesAttributeWithSpaces()
+    {
+        var tokens = tokenizer.Tokenize(
+            """<div class="hello world">Test</div>"""
+        );
+
+        Assert.Equal(
+            "hello world",
+            tokens[0].Attributes["class"]
+        );
+    }
+
+    [Fact]
+    public void ParsesSingleQuotedAttributes()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<a href='https://example.com'>Link</a>"
+        );
+
+        Assert.Equal(
+            "https://example.com",
+            tokens[0].Attributes["href"]
+        );
+    }
+
+    [Fact]
+    public void ParsesBooleanAttribute()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<input disabled>"
+        );
+
+        Assert.True(tokens[0].Attributes.ContainsKey("disabled"));
+        Assert.Null(tokens[0].Attributes["disabled"]);
+    }
+
+    [Fact]
+    public void ParsesMultipleSpaces()
+    {
+        var tokens = tokenizer.Tokenize(
+            """<div     class="test"      id="main">Hello</div>"""
+        );
+
+        Assert.Equal("test", tokens[0].Attributes["class"]);
+        Assert.Equal("main", tokens[0].Attributes["id"]);
+    }
+
+    [Fact]
+    public void ParsesTabs()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<div\tclass=\"test\"\tid=\"main\">"
+        );
+
+        Assert.Equal("test", tokens[0].Attributes["class"]);
+        Assert.Equal("main", tokens[0].Attributes["id"]);
+    }
+
+    [Fact]
+    public void ParsesNewlines()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<div\nclass=\"test\"\nid=\"main\">"
+        );
+
+        Assert.Equal("test", tokens[0].Attributes["class"]);
+        Assert.Equal("main", tokens[0].Attributes["id"]);
+    }
+
+    [Fact]
+    public void ParsesUnquotedAttribute()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<meta charset=UTF-8>"
+        );
+
+        Assert.Equal(
+            "UTF-8",
+            tokens[0].Attributes["charset"]
+        );
+    }
+
+    [Fact]
+    public void ParsesEmptyAttribute()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<input value=\"\">"
+        );
+
+        Assert.Equal(
+            "",
+            tokens[0].Attributes["value"]
+        );
+    }
+
+    [Fact]
+    public void ParsesMultipleBooleanAttributes()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<input checked disabled readonly>"
+        );
+
+        Assert.True(tokens[0].Attributes.ContainsKey("checked"));
+        Assert.True(tokens[0].Attributes.ContainsKey("disabled"));
+        Assert.True(tokens[0].Attributes.ContainsKey("readonly"));
+    }
+
+    [Fact]
+    public void ParsesEmptyElement()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<div></div>"
+        );
+
+        Assert.Equal(2, tokens.Count);
+
+        Assert.Equal(TokenType.StartTag, tokens[0].Type);
+        Assert.Equal(TokenType.EndTag, tokens[1].Type);
+    }
+
+    [Fact]
+    public void ParsesNestedTags()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<div><span>Hello</span></div>"
+        );
+
+        Assert.Equal(5, tokens.Count);
+
+        Assert.Equal("div", tokens[0].Value);
+        Assert.Equal(TokenType.StartTag, tokens[0].Type);
+        Assert.Equal("span", tokens[1].Value);
+        Assert.Equal(TokenType.StartTag, tokens[1].Type);
+        Assert.Equal("Hello", tokens[2].Value);
+        Assert.Equal(TokenType.Text, tokens[2].Type);
+        Assert.Equal("span", tokens[3].Value);
+        Assert.Equal(TokenType.EndTag, tokens[3].Type);
+        Assert.Equal("div", tokens[4].Value);
+        Assert.Equal(TokenType.EndTag, tokens[4].Type);
+    }
+
+    [Fact]
+    public void ParsesGreaterThanInsideQuotedAttribute()
+    {
+        var tokens = tokenizer.Tokenize(
+            """<div title="1 > 0"></div>"""
+        );
+
+        Assert.Equal(
+            "1 > 0",
+            tokens[0].Attributes["title"]
+        );
+    }
+
+    [Fact]
+    public void ParsesLessThanInsideQuotedAttribute()
+    {
+        var tokens = tokenizer.Tokenize(
+            """<div title="1 < 2"></div>"""
+        );
+
+        Assert.Equal(
+            "1 < 2",
+            tokens[0].Attributes["title"]
+        );
+    }
+
+    [Fact]
+    public void ParsesSlashSelfClosingTag()
+    {
+        var tokens = tokenizer.Tokenize(
+            """<img src="test.png" />"""
+        );
+
+        Assert.Single(tokens);
+
+        Assert.Equal(
+            TokenType.SelfClosingTag,
+            tokens[0].Type
+        );
+
+        Assert.Equal(
+            "test.png",
+            tokens[0].Attributes["src"]
+        );
+
+        Assert.Single(tokens[0].Attributes);
+    }
+
+    [Fact]
+    public void ParsesSelfClosingBooleanAttribute()
+    {
+        var tokens = tokenizer.Tokenize(
+            "<input disabled />"
+        );
+
+        Assert.Single(tokens);
+
+        Assert.Equal(
+            TokenType.SelfClosingTag,
+            tokens[0].Type
+        );
+
+        Assert.True(
+            tokens[0].Attributes.ContainsKey("disabled")
+        );
+
+        Assert.Null(
+            tokens[0].Attributes["disabled"]
+        );
+
+        Assert.Single(tokens[0].Attributes);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Browser;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Browser browser = new();
+        await browser.StartAsync(); 
+    }
+}
+
+public class Browser
+{
+    readonly HttpClient client = new();
+    readonly HtmlTokenizer tokenizer = new();
+
+    public async Task StartAsync()
+    {
+        try
+        {
+            string responseBody = await client.GetStringAsync("https://example.com");
+
+            foreach (Token token in tokenizer.Tokenize(responseBody))
+            {
+                Console.WriteLine(token);
+            }
+
+            Console.WriteLine(responseBody);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Fehler: {e.Message}");
+        }
+    }
+}
+
+public enum TokenType
+{
+    StartTag,
+    EndTag,
+    SelfClosingTag,
+    Text,
+    Doctype,
+    Comment
+}
+
+public class Token(TokenType type, string value, Dictionary<string, string?> attributes)
+{
+    public TokenType Type { get; } = type;
+    public string Value { get; } = value;
+    public Dictionary<string, string?> Attributes { get; } = attributes;
+
+    public override string ToString()
+    {
+        return $"{Type}: {Value}\nAttributes: {string.Join(", ", Attributes.Select(kv => $"{kv.Key}={kv.Value}"))}\n";
+    }
+}
+
+public class TagInfo
+{
+    public string Name { get; set; } = "";
+    public Dictionary<string, string?> Attributes { get; } = [];
+}
+
+public class HtmlTokenizer
+{
+    readonly List<string> selfClosingTags =
+    [
+        "area", "base", "br", "col", "embed", "hr", "img", "input",
+        "link", "meta", "param", "source", "track", "wbr"
+    ];
+
+    public static TagInfo ParseTag(string tag)
+    {
+        TagInfo result = new();
+
+        int i = 0;
+
+        // read tagname
+        while (i < tag.Length && !char.IsWhiteSpace(tag[i]))
+        {
+            result.Name += tag[i];
+            i++;
+        }
+
+        while (i < tag.Length)
+        {
+            while (i < tag.Length && char.IsWhiteSpace(tag[i]))
+                i++;
+
+            if (i >= tag.Length)
+                break;
+
+            string attributeName = "";
+
+            while (
+                i < tag.Length &&
+                tag[i] != '=' &&
+                !char.IsWhiteSpace(tag[i])
+            )
+            {
+                attributeName += tag[i];
+                i++;
+            }
+
+            while (i < tag.Length && char.IsWhiteSpace(tag[i]))
+                i++;
+
+            // bool attributes
+            if (i >= tag.Length || tag[i] != '=')
+            {
+                result.Attributes[attributeName] = null;
+                continue;
+            }
+
+            i++; // '='
+
+            while (i < tag.Length && char.IsWhiteSpace(tag[i]))
+                i++;
+
+            string attributeValue = "";
+
+            if (i < tag.Length && (tag[i] == '"' || tag[i] == '\''))
+            {
+                char quote = tag[i];
+                i++;
+
+                while (i < tag.Length && tag[i] != quote)
+                {
+                    attributeValue += tag[i];
+                    i++;
+                }
+
+                if (i < tag.Length)
+                    i++;
+            }
+            else
+            {
+                while (
+                    i < tag.Length &&
+                    !char.IsWhiteSpace(tag[i])
+                )
+                {
+                    attributeValue += tag[i];
+                    i++;
+                }
+            }
+
+            result.Attributes[attributeName] = attributeValue;
+        }
+
+        return result;
+    }
+
+    public List<Token> Tokenize(string html)
+    {
+        List<Token> tokens = new();
+
+        int i = 0;
+
+        while (i < html.Length)
+        {
+            if (html[i] == '<')
+            {
+                int end = i + 1;
+                bool inQuotes = false;
+                char quote = '\0';
+
+                while (end < html.Length)
+                {
+                    char c = html[end];
+
+                    if (!inQuotes)
+                    {
+                        if (c == '"' || c == '\'')
+                        {
+                            inQuotes = true;
+                            quote = c;
+                        }
+                        else if (c == '>')
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (c == quote)
+                        {
+                            inQuotes = false;
+                        }
+                    }
+
+                    end++;
+                }
+
+                string tag = html.Substring(i + 1, end - i - 1).TrimEnd();
+
+                if (tag.EndsWith('/'))
+                    tag = tag[..^1].TrimEnd();
+
+                TagInfo info = ParseTag(tag);
+
+                if (tag.ToLower().StartsWith("!doctype"))
+                {
+                    tokens.Add(new Token(
+                        TokenType.Doctype,
+                        tag,
+                        []
+                    ));
+                }
+                else if (tag.StartsWith("!--") && tag.EndsWith("--"))
+                {
+                    tokens.Add(new Token(
+                        TokenType.Comment,
+                        tag.Substring(3, tag.Length - 5),
+                        []
+                    ));
+                }
+                else if (tag.StartsWith('/'))
+                {
+                    tokens.Add(new Token(
+                        TokenType.EndTag,
+                        info.Name[1..],
+                        info.Attributes
+                    ));
+                }
+                else if (selfClosingTags.Contains(info.Name.ToLowerInvariant()))
+                {
+                    tokens.Add(new Token(
+                        TokenType.SelfClosingTag,
+                        info.Name,
+                        info.Attributes
+                    ));
+                }
+                else
+                {
+                    tokens.Add(new Token(
+                        TokenType.StartTag,
+                        info.Name,
+                        info.Attributes
+                    ));
+                }
+
+                i = end + 1;
+            }
+            else
+            {
+                int start = i;
+
+                while (i < html.Length && html[i] != '<')
+                {
+                    i++;
+                }
+
+                string text = html.Substring(start, i - start);
+
+                if (!string.IsNullOrWhiteSpace(text))
+                {
+                    tokens.Add(new Token(
+                        TokenType.Text,
+                        text.Trim(),
+                        []
+                    ));
+                }
+            }
+        }
+
+        return tokens;
+    }
+}
+
+public abstract class Node
+{
+    public Node? Parent { get; set; }
+    public List<Node> Children { get; } = [];
+}
+
+public class ElementNode(string tagName) : Node
+{
+    public string TagName { get; } = tagName;
+
+    public Dictionary<string, string> Attributes { get; } = [];
+}
+
+public class TextNode(string text) : Node
+{
+    public string Text { get; set; } = text;
+}
